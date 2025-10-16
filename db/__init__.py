7df6ec9f28c40f0cf3afbaf39e39d13b3faa4729
wxp <wxp@example.com>
2025-10-16 12:09:02 +0800

    优化

--- db/__init__.py
+++ db/__init__.py
@@ -14,7 +14,7 @@ def save_ocr_data(tag, post_title: str, collect_time: str, ocr_data: List[str], 
     保存OCR识别数据到数据库
     :param tag: 标签名称
     :param post_title: 文件名（作品标题）
-    :param collect_time: 采集时间
+    :param collect_time: OCR采集时间
     :param ocr_data: OCR识别的数据列表
     :param index_mapping_data: 字段名列表
     """
@@ -26,27 +26,27 @@ def save_ocr_data(tag, post_title: str, collect_time: str, ocr_data: List[str], 
 
     # print(escaped_fields)
 
-    # 创建表，使用作品标题和采集时间作为联合主键
+    # 创建表，使用作品标题和OCR采集时间作为联合主键
     create_table_sql = f'''
         CREATE TABLE IF NOT EXISTS s_xhs_{tag}_ocr (
             "作品ID" INTEGER PRIMARY KEY AUTOINCREMENT,
             "设备IP" TEXT,
             "作品标题" TEXT,
-            "采集日期" TEXT,
-            "采集时间" TEXT,
+            "截图采集日期" TEXT,
+            "OCR采集时间" TEXT,
             {(' TEXT, '.join(escaped_fields)) + ' TEXT' if escaped_fields else ''},
-            UNIQUE("作品标题", "采集时间")
+            UNIQUE("作品标题", "OCR采集时间")
         )
     '''
 
     cursor.execute(create_table_sql)
     conn.commit()
 
-    # 使用 INSERT OR IGNORE 语句，当作品标题和采集时间都相同时不插入
-    table_len = len(index_mapping_data) + 4 # 4 是指设备IP、作品标题、采集日期、采集时间（这个4个字段）
+    # 使用 INSERT OR IGNORE 语句，当作品标题和OCR采集时间都相同时不插入
+    table_len = len(index_mapping_data) + 4 # 4 是指设备IP、作品标题、截图采集日期、OCR采集时间（这个4个字段）
     cursor.execute(f'''
         INSERT OR IGNORE INTO s_xhs_{tag}_ocr (
-            "设备IP","作品标题", "采集日期","采集时间", {','.join(escaped_fields)}
+            "设备IP","作品标题", "截图采集日期","OCR采集时间", {','.join(escaped_fields)}
         ) VALUES ({','.join(['?' for _ in range(table_len)])})
     ''', (
         ip_port_dir, post_title, date_dir, collect_time,
