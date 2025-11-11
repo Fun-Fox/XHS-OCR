6466a591ff248abe16f2f382135641c596d9eaef
Your Name <you@example.com>
2025-11-11 13:52:16 +0800

    优化

--- core/logger.py
+++ core/logger.py
@@ -1,4 +1,5 @@
 
 from loguru import logger
 # 配置日志文件
-logger.add("logs/run_{time}.log", rotation="100 MB", encoding="utf-8")
+logger.add("logs/run_{time}.log", rotation="100 MB", encoding="utf-8", retention="3 days")
+
